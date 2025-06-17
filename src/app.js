2a14413b87a8639507cc1529cb75b25b55c4875a
ruiztomas <tomialeruiz@gmail.com>
2025-06-17 15:28:48 -0300

    ...

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express=require("express");
-const productsRouter=require("./routes/products-router.js");
-const cartsRouter=require("./routes/carts-router.js");
-
 const app=express();
+
+const productsRouter=require("./routes/products.js");
+const cartsRouter=require("./routes/carts.js");
+
 app.use(express.json());
 
 app.use('/api/products', productsRouter);
